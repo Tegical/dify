a0e901ed2f44b6bcd419df52d5b46b83152b479b
zhouzewei <17255546+CharZhou@users.noreply.github.com>
2025-11-24 15:45:39 +0800

    chore: apply UI customizations based on release/1.10.0
    - Comment out header logo, workspace nav and account dropdown
    - Comment out footer community links
    - Comment out 'powered by' branding in chat and text generation components
    - Update Dockerfile to use Harbor registry for Node.js base image

--- web/app/components/share/text-generation/index.tsx
+++ web/app/components/share/text-generation/index.tsx
@@ -585,22 +585,22 @@ const TextGeneration: FC<IMainProps> = ({
           )}
         </div>
         {/* powered by */}
-        {!customConfig?.remove_webapp_brand && (
-          <div className={cn(
-            'flex shrink-0 items-center gap-1.5 bg-components-panel-bg py-3',
-            isPC ? 'px-8' : 'px-4',
-            !isPC && resultExisted && 'rounded-b-2xl border-b-[0.5px] border-divider-regular',
-          )}>
-            <div className='system-2xs-medium-uppercase text-text-tertiary'>{t('share.chat.poweredBy')}</div>
-            {
-              systemFeatures.branding.enabled && systemFeatures.branding.workspace_logo
-                ? <img src={systemFeatures.branding.workspace_logo} alt='logo' className='block h-5 w-auto' />
-                : customConfig?.replace_webapp_logo
-                  ? <img src={`${customConfig?.replace_webapp_logo}`} alt='logo' className='block h-5 w-auto' />
-                  : <DifyLogo size='small' />
-            }
-          </div>
-        )}
+        {/*{!customConfig?.remove_webapp_brand && (*/}
+        {/*  <div className={cn(*/}
+        {/*    'flex shrink-0 items-center gap-1.5 bg-components-panel-bg py-3',*/}
+        {/*    isPC ? 'px-8' : 'px-4',*/}
+        {/*    !isPC && resultExisted && 'rounded-b-2xl border-b-[0.5px] border-divider-regular',*/}
+        {/*  )}>*/}
+        {/*    <div className='system-2xs-medium-uppercase text-text-tertiary'>{t('share.chat.poweredBy')}</div>*/}
+        {/*    {*/}
+        {/*      systemFeatures.branding.enabled && systemFeatures.branding.workspace_logo*/}
+        {/*        ? <img src={systemFeatures.branding.workspace_logo} alt='logo' className='block h-5 w-auto' />*/}
+        {/*        : customConfig?.replace_webapp_logo*/}
+        {/*          ? <img src={`${customConfig?.replace_webapp_logo}`} alt='logo' className='block h-5 w-auto' />*/}
+        {/*          : <DifyLogo size='small' />*/}
+        {/*    }*/}
+        {/*  </div>*/}
+        {/*)}*/}
       </div>
       {/* Result */}
       <div className={cn(
